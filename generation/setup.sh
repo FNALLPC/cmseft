9cf4bec4f9ac0aa37e5ab64b0347364e56ff2625
Nick Smith <nick.smith@cern.ch>
2023-08-24 13:53:11 -0500

    approximate setup?

--- generation/setup.sh
+++ generation/setup.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
 
-# git clone genproductions?
-# setup a CMSSW release?
+if [ ! -d genproductions ]; then
+  git clone https://github.com/cms-sw/genproductions -b mg265UL
+fi
+
+# in case this is not already done, setup cms packaging commands
+. /cvmfs/cms.cern.ch/cmsset_default.sh
+
+if [ ! -d CMSSW_10_6_27 ]; then
+  cmsrel CMSSW_10_6_27
+  pushd CMSSW_10_6_27/src
+  cmsenv
+  git cms-init
+  # git cms-addpkg PhysicsTools/NanoAOD
+  # setup weights table producer
+  # scram b -j4
+  popd
+fi
+
+# ensure environment if running again
+pushd CMSSW_10_6_27/src && cmsenv && popd
