32177d83a50a61d8a72e2b3506c10e05459fe814
kmohrman <kelci.mohrman@cern.ch>
2023-08-28 23:13:27 +0200

    Update setup

--- histograms/setup.sh
+++ histograms/setup.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-# create TopCoffea environment?
+# Set up the environment
+unset PYTHONPATH # To avoid conflicts 
+conda create --name coffea-env -c conda-forge --strict-channel-priority -y python=3.9 numpy=1.23.5 coffea # Create conda env, took like 50m on lxplus... 
+conda activate coffea-env
+
+# Install the topcoffea dependency
+git clone -b new_histEFT https://github.com/btovar/topcoffea/tree/new_histEFT
+cd topcoffea
+pip install -e .
+cd ..
+
+# Get the example data file
+wget -nc http://www.crc.nd.edu/~kmohrman/files/root_files/for_tutorial/from_daniel/nanogen_small.root
