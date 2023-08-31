8bead23826f57106408f2f409c37470c0ba20ed7
Nick Smith <nick.smith@cern.ch>
2023-08-30 19:23:09 -0500

    Use frozen env

--- histograms/setup.sh
+++ histograms/setup.sh
@@ -3,19 +3,17 @@
 # Set up the environment
 unset PYTHONPATH # To avoid conflicts 
 
-if ! which mamba 2>/dev/null; then
-  if ! which conda 2>/dev/null; then
-    echo "Conda not found, please install mambaforge by following the instructions"
-    echo " at https://github.com/conda-forge/miniforge#install"
-  else
-    conda activate base
-    conda install -c conda-forge -y mamba
-  fi
+if [ "$(which conda 2>/dev/null)" == "" ]; then
+  echo "Conda not found, please install mambaforge by following the instructions"
+  echo " at https://github.com/conda-forge/miniforge#install"
+elif [ "$(which conda 2>/dev/null)" == "/usr/bin/conda" ]; then
+  echo "Conda found, but not where you want it! Please install mambaforge by following the instructions"
+  echo " at https://github.com/conda-forge/miniforge#install"
 fi
 
-conda activate base
-if ! mamba env list|grep -q coffea-env; then
-  mamba create --name coffea-env -c conda-forge --strict-channel-priority -y python=3.9 numpy=1.23.5 coffea
+if ! conda env list|grep -q coffea-env; then
+  # mamba create --name coffea-env -c conda-forge --strict-channel-priority -y python=3.9 numpy=1.23.5 coffea
+  conda env create -f environment.yml
 fi
 conda activate coffea-env
 
